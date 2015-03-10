335243879ab793d6c809f885338d03cc705ff738
gadaR <gada_rezgui@hotmail.fr>
2015-03-10 14:43:04 +0100

    add some constant and test argv values

--- carrefour.c
+++ carrefour.c
@@ -25,11 +25,21 @@ int main (int argc, char ** argv){
         /*** automatic mode ***/
         if (strcmp(argv[i],"-a")==0) {
             puts("auto\n");
-
+            printf("genere voiture avec au max %d ms d'ecart entre 2 voitures\n", atoi(argv[i+1]));
             
         }
         if (strcmp(argv[i],"-t")==0) {
-            printf("temps %d\n", atoi(argv[i+1]));
+            printf("temps d'attente aux feux %d ms\n", atoi(argv[i+1]));
+            
+            //tempsMax=atoi(argv[i+1])
+        }
+        if (strcmp(argv[i],"-n")==0) {
+            printf("nb max voiture %d\n", atoi(argv[i+1]));
+            
+            //tempsMax=atoi(argv[i+1])
+        }
+        if (strcmp(argv[i],"-f")==0) {
+            printf("file name %s\n", argv[i+1]);
             
             //tempsMax=atoi(argv[i+1])
         }
--- carrefour.h
+++ carrefour.h
@@ -9,5 +9,11 @@
 #define ____carrefour__
 
 #include <stdio.h>
+/***********************************
+ * Here we will find mainly consant
+ **********************************/
+
+#define PRIMARY_ROUTE 1
+#define SECONDARY_ROUTE 2
 
 #endif /* defined(____carrefour__) */
